65f50f7f753f11a692e263a952aa05aa9c661c76
sunnykumaragrawal <sunny.agarwal865@gmail.com>
2023-04-21 21:15:50 +0530

    blog edited

--- resources/views/blog/keyword/freetools.blade.php
+++ resources/views/blog/keyword/freetools.blade.php
@@ -1,18 +1,14 @@
 @extends('base')
 
 @section('title')
-Free tool for keyword research - keyword tool by google @stop
+Free tool for keyword research - 7 Amazing keyword tool
+@stop
 
 @section('meta_desc')
-In this article I have mentioned free keywords research tools for you. You can easily use them. @stop
+In this article I have mentioned best free keyword research tools for keyword research. I have also explained how to use these tools in easy steps. @stop
 
 @section('meta_keywords')
-keyword tool,
-keyword research tool,
-free tool for keyword research,
-free keyword tool,
-free keyword research tool,
-best keyword research tool,
+free tool for keyword research,keyword research tool,keyword search tool,free keyword tool,best keyword research tool,best free keyword research tool,best free tools for keyword research,keyword research tools for seo,keyword tool,
 @stop
 
 @section('content')
@@ -23,13 +19,40 @@ best keyword research tool,
         <div class="col-md-10">
           <article class="single-blog-item">
             <div class="single-blog-title">
-              <h1>Free Tools for keyword research in seo</h1>
+              <h1>Free tool for keyword research - 7 Amazing keyword tool</h1>
             </div>
             <div class="single-blog-img">
               <img src="{{asset('assets/img/blog/thumbnail/keyword-research-tools.png')}}" alt="keyword research tools" class="img-fluid">
             </div>
+            <div class="single-blog-content">
+              <h3>What is keyword research tool?</h3>
+              <p>Keyword research tool is a way to find the right keywords for your web page that people can use to reach your website.</p>
+            </div>
+            <div class="single-blog-content">
+              <h3>What are the benefits of keyword tool?</h3>
+              <p>There are lots of benefits using keyword research tool:</p>
+              <ul class="sbc-list">
+                <li>To check search volume</li>
+                <li>To check competition level</li>
+                <li>To get keyword ideas</li>
+                <li>To get trending keywords</li>
+              </ul>
+            </div>
+            <div class="single-blog-content">
+              <h3>Best free tools for keyword research</h3>
+              <p>There are lots of keyword research tools available. But we are mentioning best free tools here:</p>
+              <ul class="sbc-list">
+                <li>Google Keyword Planner</li>
+                <li>Keyword tool io</li>
+                <li>Keyword Everywhere</li>
+                <li>Keyword sheeter</li>
+                <li>Keyword surfer</li>
+                <li>Google trends</li>
+                <li>Answer the public</li>
+              </ul>
+            </div>
             <div class="single-blog-points">
-              <h3>6 free tools for keyword research:</h3>
+              <h3>7 best free tools for keyword research:</h3>
               <div class="single-blogs-steps">
                 <p><b>1. Google Keyword Planner:</b> This keyword research tool is owned by google. With the help of this tool you can get the average monthly search volume, competition level, and also related keywords idea.</p>
                 <div class="visit-link"><a href="{{route('tool.googlekeywordplanner')}}" target="_blank">How to use google keyword planner</a></div>
@@ -73,8 +96,8 @@ best keyword research tool,
     "@type": "WebPage",
     "@id": "https://sunnyagrawal.in/blog/free-keyword-research-tools"
   },
-  "headline": "Best free keyword research tools by google",
-  "description": "In this blog I have mentioned free keyword tools , some are owned by google and some by others",
+  "headline": "Free tool for keyword research - 7 Amazing keyword tool",
+  "description": "In this article I have mentioned best free keyword research tools for keyword research. I have also explained how to use these tools in easy steps.",
   "image": "https://sunnyagrawal.in/assets/img/blog/thumbnail/keyword-research-tools.png",  
   "author": {
     "@type": "Person",
@@ -82,7 +105,7 @@ best keyword research tool,
     "url": "https://www.facebook.com/sunny.agarwal.395669/"
   },  
   "datePublished": "2023-04-01",
-  "dateModified": "2023-04-07"
+  "dateModified": "2023-04-21"
 }
 </script>
 @endsection
