6cb8c1877688144b0e0f0739c49692f73dd74d13
sunnykumaragrawal <sunny.agarwal865@gmail.com>
2023-04-22 20:14:56 +0530

    backlink edited

--- resources/views/blog/backlink/howtocreate.blade.php
+++ resources/views/blog/backlink/howtocreate.blade.php
@@ -1,7 +1,7 @@
 @extends('base')
 
 @section('title')
-How to create backlinks for my website - Easy guide
+How to create backlinks for my website - step by step guide
 @stop
 
 @section('meta_desc')
@@ -9,8 +9,7 @@ Backlinks are one of the most powerful thing in seo. If you want to get more tra
 @stop
 
 @section('meta_keywords')
-how to create backlinks,
-what are backlinks for seo,
+backlinks,backlink in seo,seo backlinks,what are backlinks,what are backlinks for seo,how to create backlinks,
 @stop
 
 @section('content')
@@ -27,35 +26,54 @@ what are backlinks for seo,
               <img src="{{asset('assets/img/blog/thumbnail/how-to-create-backlinks.png')}}" alt="How to create backlinks for my website" class="img-fluid">
             </div>
             <div class="single-blog-content">
-              <h3>What are backlinks for seo?</h3>
-              <p>Backlinks are the links from others website pointing to your website. The more you have quality backlinks the more trust will develop in the eye of search engine. Backlink is one of the most important ranking factor in seo , so we should not neglect it.</p>
+              <h3>What are backlinks?</h3>
+              <p>Backlinks are the links from others website pointing to your website. Backlink is a good way to increase traffic on your website. It also help us to increase trust and popularity of our business.</p>
             </div>
             <div class="single-blog-content">
-              <h3>Methods to get backlinks:</h3>
-              <p>There are lots of ways to getting backlinks , we are mentioning here the top suggested techniques:</p>
+              <h3>What are backlinks for seo?</h3>
+              <p>Backlink is one of the most important ranking factor in seo. Approximately 20-25% of your ranking depent upon your backlinks. It means if you have quality backlinks , your website ranking will up 20-25%. The more you have quality backlinks the more trust will develop in the eye of search engine(high EAT score), so we should not neglect it.</p>
+            </div>
+            <div class="single-blog-content">
+              <h3>How to create backlinks for my website:</h3>
+              <p>There are lots of ways to get backlinks , we are mentioning here the top suggested techniques:</p>
               <ul class="sbc-list">
-                <li>Analyse competitors backlinks</li>
-                <li>Broken-link building</li>
-                <li>Guest articles</li>
+                <li>Competitor Analysis</li>
+                <li>Broken link building</li>
+                <li>Guest posting</li>
+                <li>Ask with your partners</li>
+                <li>Backlinks through infographics</li>
                 <li>Backlinks from Social Media</li>
+                <li>Build Internal links</li>
               </ul>
             </div>
             <div class="single-blog-points">
               <h3>Explaination how these methods work:</h3>
               <div class="single-blogs-steps">
-                <p><b>1. Analyze competitors backlinks:</b> The best method to get quality backlinks is to analyze your competitors backlinks. Check that from where they have earned backlinks. Contact them and ask for a link in exchange of anything you can do for them.</p>
+                <p><b>1. Competitor Analysis:</b> The best method to get quality backlinks is to analyze your competitors backlinks. Check that from where they have earned backlinks. Contact them and ask for a link in exchange of anything you can do for them.</p>
                 <div class="visit-link"><a href="https://monitorbacklinks.com/" target="_blank">Tool to check backlinks</a></div>
               </div>
               <div class="single-blogs-steps">
-                <p><b>2. Broken-link building:</b> This is a very popular method of getting backlinks. In this method you have to find relevant websites in your niche that have resources pages but that no longer exist. Ask them to replace that page with yours that have similar resources.</p>
+                <p><b>2. Broken link building:</b> This is a very popular method of getting backlinks. In this method you have to find relevant websites in your niche that have resources pages but that no longer exist. Ask them to replace that page with yours that have similar resources.</p>
                 <div class="visit-link"><a href="https://chrome.google.com/webstore/detail/website-broken-link-and-4/kkjfobdnekhdpmgomkpeibhlnmcjgian" target="_blank">Tool to check broken link</a></div>
               </div>
               <div class="single-blogs-steps">
-                <p><b>3. Guest Articles:</b> Guest blogging is one of the most effective way to reach new audience. For this you have to publish your article on others website who accept guest articles. You can use below query to find guest blogging websites: "your keyword + inurl:write-for-us"</p>
+                <p><b>3. Guest Posting:</b> Guest posting is one of the most effective way to reach new audience. For this you have to publish your article on others website who accept guest articles. You can use below query to find guest blogging websites: "your keyword + inurl:write-for-us"</p>
                 <div class="visit-link"></div>
               </div>
               <div class="single-blogs-steps">
-                <p><b>4. Backlinks from Social Media:</b> Every social media platform provide a backlink for us. we can create backlink for our business there.<br>For example: In instagram you can put your link under "Bio Section", In youtube you can put your link in "Trailer video description". These links will work as backlinks for your website.</p>
+                <p><b>4. Ask with your partners:</b> If you have partners , suppliers or distributors in your business. You can ask for a link from their websites to your website.</p>
+                <div class="visit-link"></div>
+              </div>
+              <div class="single-blogs-steps">
+                <p><b>5. Backlinks through infographics:</b> Everyone loves visual data(images , videos, graphs etc). Try to create visual content and share it in infographics websites. Remember that content must be unique , trending and easy to understand.</p>
+                <div class="visit-link"></div>
+              </div>
+              <div class="single-blogs-steps">
+                <p><b>6. Backlinks from Social Media:</b> Every social media platform provide a backlink for us. we can create backlink for our business there.<br>For example: In instagram you can put your link under "Bio Section", In youtube you can put your link in "Trailer video description". These links will work as backlinks for your website.</p>
+                <div class="visit-link"></div>
+              </div>
+              <div class="single-blogs-steps">
+                <p><b>7. Build Internal links:</b> Sometimes internal links also work as backlinks. so try to create proper internal links with right hyperlink text. It will also help users to navigate properly , Thus a better experience for them.</p>
                 <div class="visit-link"></div>
               </div>
             </div>
@@ -73,7 +91,7 @@ what are backlinks for seo,
     "@type": "WebPage",
     "@id": "https://sunnyagrawal.in/blog/how-to-create-backlinks"
   },
-  "headline": "How to create backlinks for my website - Easy guide",
+  "headline": "How to create backlinks for my website - step by step guide",
   "description": "Backlinks are one of the most powerful thing in seo. If you want to get more traffic and ranker higher. You have to go through link-building process. ",
   "image": "https://sunnyagrawal.in/assets/img/blog/thumbnail/how-to-create-backlinks.png",  
   "author": {
@@ -82,7 +100,7 @@ what are backlinks for seo,
     "url": "https://www.facebook.com/sunny.agarwal.395669/"
   },  
   "datePublished": "2023-04-01",
-  "dateModified": "2023-04-07"
+  "dateModified": "2023-04-22"
 }
 </script>
 @endsection
